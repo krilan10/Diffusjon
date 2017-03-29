c09cceceff2758fec96080e19d42e42ee0755392
Kristian Landsverk <kristian.landsverk@gmail.com>
2017-03-29 17:35:34 +0200

    Diffusjon

---
+++ Diffusjon/src/diffusjon/Field.java
@@ -0,0 +1,41 @@
+package diffusjon;
+
+/**
+ * Created by Kristian on 29.03.2017.
+ */
+public class Field {
+    private int col;
+    private int row;
+    private int numberOf;
+
+    public Field(int col, int row){
+        this.col = col;
+        this.row = row;
+
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public int getNumberOf() {
+        return numberOf;
+    }
+
+    public void setNumberOf(int numberOf) {
+        this.numberOf = numberOf;
+    }
+}
